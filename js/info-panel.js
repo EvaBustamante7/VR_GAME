32867fbe26d7eff0e34cccf0a79bb09c909e69c8
Eva Bustamante <evabrodriguez7@gmail.com>
2023-07-26 20:03:00 +0200

    arreglo ingo-panle.js

--- js/info-panel.js
+++ js/info-panel.js
@@ -1,81 +1,74 @@
 /* global AFRAME */
 AFRAME.registerComponent('info-panel', {
-    init: function () {
-      var buttonEls = document.querySelectorAll('.menu-button');
-      var fadeBackgroundEl = this.fadeBackgroundEl = document.querySelector('#fadeBackground');
-  
-      this.movieImageEl;
-      this.movieTitleEl = document.querySelector('#movieTitle');
-      this.movieDescriptionEl = document.querySelector('#movieDescription');
-  
-      this.movieInfo = {
-        karigurashiButton: {
-          title: 'Duck Hunt',
-          imgEl: document.querySelector('#karigurashiMovieImage'),
-          description: 'Abate todos los patos que puedas en el menor tiempo posible',
-          
-        },
-        kazetachinuButton: {
-          title: 'The best hunters',
-          imgEl: document.querySelector('#kazetachinuMovieImage'),
-          description: 'Zori    2300\nSol      3400'
-                       
-        },
-        ponyoButton: {
-          title: 'Ponyo (2003)',
-          imgEl: document.querySelector('#ponyoMovieImage'),
-          description: 'It is the eighth film Miyazaki directed for Studio Ghibli, and his tenth overall. The film tells the story of Ponyo (Nara), a goldfish who escapes from the ocean and is rescued by a five-year-old human boy, Sōsuke (Doi) after she is washed ashore while trapped in a glass jar.'
-        }
+  init: function () {
+    var buttonEls = document.querySelectorAll('.menu-button');
+    var fadeBackgroundEl = this.fadeBackgroundEl = document.querySelector('#fadeBackground');
+
+    this.movieImageEl;
+    this.movieTitleEl = document.querySelector('#movieTitle');
+    this.movieDescriptionEl = document.querySelector('#movieDescription');
+
+    this.movieInfo = {
+      karigurashiButton: {
+        title: 'Duck Hunt',
+        imgEl: document.querySelector('#karigurashiMovieImage'),
+        description: 'Abate todos los patos que puedas en el menor tiempo posible',
         
-      };
-
-      var oculusControlsEl = document.querySelector('[oculus-touch-controls]');
-      if (oculusControlsEl) {
-        oculusControlsEl.addEventListener('triggerdown', this.onTriggerDown.bind(this));
-    }
-  
-      this.onMenuButtonClick = this.onMenuButtonClick.bind(this);
-      this.onBackgroundClick = this.onBackgroundClick.bind(this);
-      this.backgroundEl = document.querySelector('#background');
-      for (var i = 0; i < buttonEls.length; ++i) {
-        buttonEls[i].addEventListener('click', this.onMenuButtonClick);
+      },
+      kazetachinuButton: {
+        title: 'The best hunters',
+        imgEl: document.querySelector('#kazetachinuMovieImage'),
+        description: 'Zori    2300\nSol      3400'
+                     
+      },
+      ponyoButton: {
+        title: 'Ponyo (2003)',
+        imgEl: document.querySelector('#ponyoMovieImage'),
+        description: 'It is the eighth film Miyazaki directed for Studio Ghibli, and his tenth overall. The film tells the story of Ponyo (Nara), a goldfish who escapes from the ocean and is rescued by a five-year-old human boy, Sōsuke (Doi) after she is washed ashore while trapped in a glass jar.'
       }
-      this.backgroundEl.addEventListener('click', this.onBackgroundClick);
-      this.el.object3D.renderOrder = 9999999;
-      this.el.object3D.depthTest = false;
-      fadeBackgroundEl.object3D.renderOrder = 9;
-      fadeBackgroundEl.getObject3D('mesh').material.depthTest = false;
-    },
-  
-    onMenuButtonClick: function (evt) {
-      var movieInfo = this.movieInfo[evt.currentTarget.id];
-  
-      this.backgroundEl.object3D.scale.set(1, 1, 1);
-  
-      this.el.object3D.scale.set(1, 1, 1);
-      if (AFRAME.utils.device.isMobile()) { this.el.object3D.scale.set(1.4, 1.4, 1.4); }
-      this.el.object3D.visible = true;
-      this.fadeBackgroundEl.object3D.visible = true;
-  
-      if (this.movieImageEl) { this.movieImageEl.object3D.visible = false; }
-      this.movieImageEl = movieInfo.imgEl;
-      this.movieImageEl.object3D.visible = true;
-  
-      this.movieTitleEl.setAttribute('text', 'value', movieInfo.title);
-      this.movieDescriptionEl.setAttribute('text', 'value', movieInfo.description);
       
-    },
-  
-    onBackgroundClick: function (evt) {
+    };
 
-      if (this.movieImageEl && this.movieImageEl.id === 'karigurashiMovieImage') {
-        window.location.href = 'game.html';
-      } else {
-        this.backgroundEl.object3D.scale.set(0.001, 0.001, 0.001);
-        this.el.object3D.scale.set(0.001, 0.001, 0.001);
-        this.el.object3D.visible = false;
-        this.fadeBackgroundEl.object3D.visible = false;
-      }
+    this.onMenuButtonClick = this.onMenuButtonClick.bind(this);
+    this.onBackgroundClick = this.onBackgroundClick.bind(this);
+    this.backgroundEl = document.querySelector('#background');
+    for (var i = 0; i < buttonEls.length; ++i) {
+      buttonEls[i].addEventListener('click', this.onMenuButtonClick);
     }
+    this.backgroundEl.addEventListener('click', this.onBackgroundClick);
+    this.el.object3D.renderOrder = 9999999;
+    this.el.object3D.depthTest = false;
+    fadeBackgroundEl.object3D.renderOrder = 9;
+    fadeBackgroundEl.getObject3D('mesh').material.depthTest = false;
+  },
 
-  });
+  onMenuButtonClick: function (evt) {
+    var movieInfo = this.movieInfo[evt.currentTarget.id];
+
+    this.backgroundEl.object3D.scale.set(1, 1, 1);
+
+    this.el.object3D.scale.set(1, 1, 1);
+    if (AFRAME.utils.device.isMobile()) { this.el.object3D.scale.set(1.4, 1.4, 1.4); }
+    this.el.object3D.visible = true;
+    this.fadeBackgroundEl.object3D.visible = true;
+
+    if (this.movieImageEl) { this.movieImageEl.object3D.visible = false; }
+    this.movieImageEl = movieInfo.imgEl;
+    this.movieImageEl.object3D.visible = true;
+
+    this.movieTitleEl.setAttribute('text', 'value', movieInfo.title);
+    this.movieDescriptionEl.setAttribute('text', 'value', movieInfo.description);
+    
+  },
+
+  onBackgroundClick: function (evt) {
+    if (this.movieImageEl && this.movieImageEl.id === 'karigurashiMovieImage') {
+      window.location.href = 'game.html';
+    } else {
+      this.backgroundEl.object3D.scale.set(0.001, 0.001, 0.001);
+      this.el.object3D.scale.set(0.001, 0.001, 0.001);
+      this.el.object3D.visible = false;
+      this.fadeBackgroundEl.object3D.visible = false;
+    }
+  }
+});
